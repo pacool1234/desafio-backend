a0e57e04823b077f2f4f14bff1ca06c07d3af3ab
GuileSoler87 <guillermosoler87@gmail.com>
2023-06-03 01:41:26 +0200

    User controller & UserType controller added

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ app.use(cors());
 app.use(express.static("./public")); //Necessary to get correct url in frontend
 
 app.use("/users", require("./routes/users"));
-app.use("/bookings", require("./routes/bookings"));
-app.use("/meetings", require("./routes/meetings"));
-app.use("/events", require("./routes/events")); // FALTABA ESTO VALENTI
+
 
 
 dbConnection();
