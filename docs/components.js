6ce56b68d5fc53ebbc6ef5dfcd190e7ffcab1edb
joanbaldo <joanbaldo@hotmail.com>
2023-06-10 20:43:08 +0200

    Components and tags and users schema

---
+++ docs/components.js
@@ -0,0 +1,26 @@
+module.exports = {
+    components: {
+        schemas: {
+            task: {
+                type: 'object',
+                properties: {
+                    _id: {
+                        type: 'objectId',
+                        description: "task identification number",
+                        example: "6201064b0028de7866e2b2c4"
+                    },
+                    title: {
+                        type: 'string',
+                        description: "task's title",
+                        example: "make an excellent readme"
+                    },
+                    completed: {
+                        type: "boolean",
+                        description: "The status of the task",
+                        example: false
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ docs/index.js
@@ -0,0 +1,11 @@
+const basicInfo = require('./basicInfo');
+const components = require('./components');
+const tags = require('./tags');
+
+module.exports = {
+...basicInfo ,
+...components ,
+...tags
+
+};
+
