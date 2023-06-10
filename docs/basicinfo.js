c61536bdfc3981a8cead6ae8b225ad4439a06ca9
joanbaldo <joanbaldo@hotmail.com>
2023-06-10 17:48:31 +0200

    Swagger components.js setup

--- docs/basicinfo.js
+++ docs/basicinfo.js
@@ -2,7 +2,7 @@ module.exports = {
     openapi:"3.0.3",
     info:{
     version:"1.0.0",
-    title:"Agora_API",
+    title:"Agora API",
     description: "API para la aplicación AGORA"
     }
     }
