b5db68e813a76520de82a9dc7ebf241dbeb226ea
GuileSoler87 <guillermosoler87@gmail.com>
2023-06-10 17:23:44 +0200

    sad

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -263,7 +263,7 @@ const UserController = {
         }
       );
       function base64UrlEncode(str) {
-        return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '').replace(/\./, '¿');
+        return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '').replace(/\./g, '¿');
      };
      const encodedToken = base64UrlEncode(recoverToken)
   
