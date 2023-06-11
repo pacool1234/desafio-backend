a32497cb3d3e26fe41dd85f635ebd6e4468b8e5a
pacool1234 <niederleytnerenciso@gmail.com>
2023-06-11 20:43:21 +0200

    minor changes

--- models/UserType.js
+++ models/UserType.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // const ObjectId = mongoose.SchemaTypes.ObjectId;
 
-const UserTypeSchema = new mongoose.Schema({
+const UserTypeSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please enter a userType"],
+      type: String,
     },
+  },
+  { timestamps: true }
+);
 
-}, { timestamps: true });
-
-
-
-const UserType = mongoose.model('UserType', UserTypeSchema);
+const UserType = mongoose.model("UserType", UserTypeSchema);
 
 module.exports = UserType;
