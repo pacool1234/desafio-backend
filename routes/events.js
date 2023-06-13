d64aa5797e93f4c12a04b1624475ffbee06995ba
joanbaldo <joanbaldo@hotmail.com>
2023-06-13 11:35:46 +0200

    EVENT endpoints getAll and getById created

--- routes/events.js
+++ routes/events.js
@@ -6,6 +6,7 @@ const {uploadEventsImg} = require('../middlewares/upload');
 
 router.post('/create',uploadEventsImg.single('img'),EventController.create)
 router.delete('/delete/:_id',EventController.delete)
+router.get('/get/:_id',EventController.getById)
 
 
 module.exports = router;
