ccadc9c50a6d51d833ecd9364416f38b8fbcbc16
GuileSoler87 <guillermosoler87@gmail.com>
2023-06-13 17:38:11 +0200

    asdsa

--- controllers/NoticeController.js
+++ controllers/NoticeController.js
@@ -43,7 +43,7 @@ const NoticeController = {
 
       async getById(req, res) {
         try {
-            const notice = await Notice.findById(req.params._id).populate('commentIds')
+            const notice = await Notice.findById(req.params._id).populate('commentIds').populate('userId')
             res.send(notice)
         } catch (error) {
             console.error(error);
