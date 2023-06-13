0ec8d62fd633bbd214860d7b76bb5cf1752252f2
GuileSoler87 <guillermosoler87@gmail.com>
2023-06-13 16:24:41 +0200

    changes routes comments

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const { authentication } = require("../middlewares/authentication");
 
 
 
-router.post('/create/:_id',authentication , CommentController.create)
+router.post('/create',authentication , CommentController.create)
 router.put('/update/:_id',authentication , CommentController.update)
 router.get('/getAll',authentication , CommentController.getAll)
 router.get('/getbyId/:_id',authentication , CommentController.getById)
